5e0149a4109ee6cf525a73b6940ebd3a28304e55
Yamingue kingatal <yamking01@gmail.com>
2022-03-27 01:28:24 +0100

    ajout du floting btn

--- player.js
+++ player.js
@@ -1,28 +1,36 @@
+var player = new Audio('./IMG_0584.MP4')
+// declavARION DES VARIABLE LIER AU DOM
 var volume = document.getElementById('volume-progress')
 var iconPlay = document.getElementById('icon')
-var player = new Audio('./IMG_0584.MP4')
 var playlist = document.querySelector('.playlist')
 var playerContainer = document.querySelector('#player')
+var closeIcon = document.querySelector('.close-icon')
 var playerTitle = document.querySelector('.player-title')
-var playerTitle = document.querySelector('.close-icon')
+var flotingBtn = document.querySelector('.floting-btn').firstElementChild
+
+//JOUER LE SONS
 volume.value = player.volume*100
 player.play()
-
-setTimeout(()=>{
-    playerContainer.classList.toggle('hide')
-    console.log(playerContainer)
-},2000)
+flotingBtn.addEventListener('click',function (ev) {
+    // if (player.paused) {
+    //     player.play()
+    // } else{
+    //     player.pause()
+    // }
+    playlist.classList.remove('hide')
+    playerContainer.style.display = 'flex'
+})
 player.addEventListener('play',function (ev) {
     iconPlay.classList ='fa-solid fa-pause'
-    //'fa-solid fa-play':
+    flotingBtn.classList ='fa-solid fa-pause fa-2x'
 })
 
 playerTitle.addEventListener('click',function () {
     console.log(playlist.classList.toggle('hide'))
-   // playlist.className.toggle('hide')
 })
  
 player.addEventListener('pause',function (ev) {
+    flotingBtn.classList ='fa-solid fa-play fa-2x'
     iconPlay.classList ='fa-solid fa-play'
     //'fa-solid fa-play':
 })
@@ -41,7 +49,8 @@ volume.addEventListener('input',function (ev) {
     console.log(player.volume)
 })
 
-icon.addEventListener('click',function (ev) {
-    
+closeIcon.addEventListener('click',function (ev) {
+    playlist.classList.add('hide')
+    playerContainer.style.display = 'none'
 })
 
